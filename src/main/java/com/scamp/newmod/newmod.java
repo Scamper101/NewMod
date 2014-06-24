5ea769de63c1d32d33a5270e8bbe26d95ead0aa0
Chris <chris.antholz@hotmail.com>
2014-06-23 20:14:31 -0500

    Signed-off-by: Chris <chris.antholz@hotmail.com>

---
+++ src/main/java/com/scamp/newmod/newmod.java
@@ -0,0 +1,31 @@
+package com.scamp.newmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="newmod", name="New Mod", version="1.7.2-1.0")
+public class newmod
+{
+    @Mod.Instance("newmod")
+  public static newmod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
